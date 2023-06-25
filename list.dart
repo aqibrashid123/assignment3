3685e7b90dc65b216116a48d0f559a835468dab2
aqibrashid123 <aqinalii66958gmail.com>
2023-06-24 22:46:50 -0700

    assignment

---
+++ list.dart
@@ -0,0 +1,162 @@
+// void main(){
+//  List studentName = ["Aqib", "Ali", "Ayan", "Bilal","Hano"];
+// print(studentName);
+
+// }
+
+
+// void main(){
+
+// List<String>  days = [];
+// days.addAll(["Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday"]);
+// print(days);
+   
+// } 
+
+// void main(){
+
+// List<String> days = ["Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday"];
+// days.removeLast();
+// days.removeLast();
+// days.removeLast();
+// days.removeLast();
+// days.removeLast();
+// days.removeLast();
+// days.removeLast();
+
+// print(days);
+
+// }
+
+// void main(){
+
+// // Creating a List
+// List number1 = [121, 12, 33, 14, 3];
+
+// // Declaring and assigning
+// // largestValue and smallestValue
+// var largestValue = number1[0];
+// var smallestValue = number1[0];
+
+// if (largestValue > smallestValue);
+// {print("largest value in the list is 3");}
+
+// if (largestValue < smallestValue);
+// {print("largest value in the list is 121");}
+
+// }
+
+// void main(){
+// Map<String, double> expenses = {
+//  'sun':3000.0,
+//  'mon':3000.0,
+//  'tue':3234.0,
+// };
+
+// if (expenses.containsKey('fri')){
+//  expenses['fri'] = 5000.0;
+// } else {
+//   expenses['fri'] = 5000.0;
+// }
+
+// print(expenses);
+
+// }
+
+// void main(){
+// List stuname =["aqib","arbaz","owais","khalid","anas","huzaifa","bilal"];
+// List newstunamelist 
+// =stuname.toSet().toList();
+// print(stuname);
+// print(newstunamelist);
+
+// }
+
+// void main(){
+// List <int> number =[1,2,3,4,5,6];
+// int n= 4;
+// List newlist = number.sublist(0,n);
+// print(newlist);
+
+// }
+
+
+// void main(){
+// //  creating a list
+// var orgList =
+// ["apple","banana","mango","watermelon"];
+//  print(orgList);
+
+// //  Reversing the list
+// var objectList = orgList.reversed ;
+
+// // creating a new list with the object
+// var reversedList = new
+// List.from(objectList);
+//  print(reversedList);
+
+// }
+
+// void main(){
+//  List<int> stuRollNo = [9,5,2,7,1,6];
+//  List<int> sortedList =
+// List.from(stuRollNo);      
+// //  Creat a new list with the same elements
+
+// sortedList.sort();
+// // sort the new list in ascending order
+
+//  print('Original List: $stuRollNo');
+//  print('Sorted List: $sortedList');
+// }
+
+// void main(){
+// Map<String, dynamic> person = {
+//  "name": "John",
+//  "age": 23,
+//  "isStudent": "ture",
+// };
+
+// bool isStudent = person["isStudent"];
+// int age = person["age"];
+
+// if (isStudent && age > 18){
+//   print("English");
+// } else {
+//   print("Not eligible");
+// }
+
+// }
+
+// void main(){
+// Map<String, dynamic> product = {
+//  "name": "Widget",
+// "price": 10.99,
+// "quantity": 0,
+//  };
+
+//  int  quantity = product["quantity"];
+
+//  if (quantity > 0) {
+//   print("in stock");
+//  } else {
+//   print("out of stock");
+//  }
+// }
+
+// void main(){
+// Map<String, dynamic> car = {
+//  "brand": "Toyota",
+//  "color": "Red",
+//  "isSedan": "true",
+// };
+
+// String color = car["color"];
+// bool isSedan = car["isSedan"];
+
+// if (color == "Red" && isSedan) {
+//  print("Match");
+//  } else {
+//  print("No match"); 
+//  }
+// }
